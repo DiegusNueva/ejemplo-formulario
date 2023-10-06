3212e279387008d4a5fd17df3f6a6bfdda069d72
Diego Alonso Molina <diegoalonso@tuniversformacion.es>
2023-10-06 09:30:12 +0200

    Cambios definitivos.

--- php/procesar_registro.php
+++ php/procesar_registro.php
@@ -1,24 +1,17 @@
 <?php
-// Verifica si se reciben datos en formato JSON
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_SERVER['CONTENT_TYPE']) && $_SERVER['CONTENT_TYPE'] === 'application/json') {
-    // Recibe el contenido JSON
-    $json_data = file_get_contents('php://input');
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Recuperar los datos del formulario
+        $nombre = $_POST["nombre"];
+        $apellidos = $_POST["apellidos"];
+        $email = $_POST["email"];
+        $password = $_POST["password"];
 
-    // Convierte el JSON en un objeto PHP
-    $data = json_decode($json_data);
-
-    // Verifica si se pudo decodificar el JSON correctamente
-    if ($data !== null) {
-        // Aquí puedes realizar la lógica de procesamiento de los datos
-        // Por ejemplo, podrías almacenar los datos en una base de datos o realizar otras operaciones
-        // En este ejemplo, simplemente los devolveremos tal como los recibimos
-        echo json_encode($data);
+        // Mostrar los datos en el navegador
+        echo "<p><strong>Nombre:</strong> $nombre</p>";
+        echo "<p><strong>Apellidos:</strong> $apellidos</p>";
+        echo "<p><strong>Correo electrónico:</strong> $email</p>";
+        // La contraseña no se muestra por razones de seguridad
     } else {
-        // Si no se pudo decodificar el JSON correctamente
-        echo json_encode(array("error" => "Error al procesar los datos JSON."));
+        echo "<p>No se han recibido datos del formulario.</p>";
     }
-} else {
-    // Si no se reciben datos en formato JSON
-    echo json_encode(array("error" => "Se esperaba recibir datos en formato JSON."));
-}
 ?>
--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,51 @@
-document.getElementById("registroForm").addEventListener("submit", function (event) {
-    const nombre = document.getElementById("nombre").value;
-    const apellidos = document.getElementById("apellidos").value;
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-
-    if (!nombre || !apellidos || !email || !password) {
-        alert("Por favor, complete todos los campos.");
-        event.preventDefault(); // Evita que el formulario se envíe
+// Validar que el nombre no esté vacío
+function validarNombre(nombre) {
+    if (nombre === "") {
+        alert("El nombre no puede estar vacío.");
+        return false;
     }
-});
+    return true;
+}
+
+// Validar que la dirección de correo electrónico sea válida
+function validarEmail(email) {
+    var regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (!regex.test(email)) {
+        alert("La dirección de correo electrónico no es válida.");
+        return false;
+    }
+    return true;
+}
+
+// Validar que la contraseña tenga al menos 8 caracteres
+function validarContraseña(contraseña) {
+    if (contraseña.length < 8) {
+        alert("La contraseña debe tener al menos 8 caracteres.");
+        return false;
+    }
+    return true;
+}
+
+// Validar todos los campos del formulario
+function validarFormulario() {
+    let nombre = document.getElementById("nombre").value;
+    let email = document.getElementById("email").value;
+    let contraseña = document.getElementById("password").value;
+
+    return validarNombre(nombre) && validarEmail(email) && validarContraseña(contraseña);
+}
+
+// Asignar la función enviarFormulario al evento onsubmit del formulario
+document.getElementById("registroForm").onsubmit = function () {
+    return validarFormulario();
+};
+
+// Enviar el formulario si todos los campos son válidos
+function enviarFormulario() {
+    if (validarFormulario()) {
+        return true; // Permite que el formulario se envíe
+    } else {
+        return false; // Evita que el formulario se envíe si la validación falla
+    }
+}
+
